7c813a44bb848eea3a744b9bc5a30483caedbdd3
Charles Chiou <samurai@selfso.com>
2023-05-13 19:42:25 +0800

    Implement a fork version of controlling espeak
    Signed-off-by: Charles Chiou <samurai@selfso.com>

--- controller/speech.cxx
+++ controller/speech.cxx
@@ -6,28 +6,66 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/times.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <string.h>
+#include <pthread.h>
 #include <math.h>
 #include <alsa/asoundlib.h>
 #include "speech.hxx"
 
 Speech::Speech()
-    : _vol(100),
-      _wpm(120),
-      _espeak(NULL)
+    :
+#if defined(ESPEAK_POPEN)
+    _espeak(NULL),
+#else
+    _pid(-1),
+    _stdin(),
+#endif
+    _vol(100),
+    _wpm(120)
 {
+    pthread_mutex_init(&_mutex, NULL);
+
     setVolume(88);
     speak("The rabbit bot is ready to see the world!");
 }
 
 Speech::~Speech()
 {
+    speak("The rabbit bot is going to sleep!");
+
+#if defined(ESPEAK_POPEN)
     if (_espeak) {
-        speak("The rabbit bot is going to sleep!");
         pclose(_espeak);
         _espeak = NULL;
     }
+#endif
+#if defined(ESPEAK_FORK)
+    if (_pid != -1) {
+        int wstatus;
+
+        close(_stdin[1]);
+        _stdin[1] = -1;
+
+        waitpid(_pid, &wstatus, 0);
+        _pid = -1;
+    }
+#endif
+
+    pthread_mutex_destroy(&_mutex);
 }
+
+#if defined(ESPEAK_FORK)
+
+static void atexit_do_nothing(void)
+{
+
+}
+
+#endif
 
 void Speech::speak(const char *message, bool immediate, int wpm)
 {
@@ -35,13 +73,19 @@ void Speech::speak(const char *message, bool immediate, int wpm)
     char cmd[256];
 
     if (message == NULL) {
-        return;
+        goto done;
     }
 
     if (wpm <= 0) {
         wpm = _wpm;
     }
 
+    snprintf(cmd, sizeof(cmd) - 1,
+             "/usr/bin/espeak -s %d 2>/dev/null", wpm);
+
+    pthread_mutex_lock(&_mutex);
+
+#if defined(ESPEAK_PIPE)
     if ((_espeak != NULL) &&
         ((immediate == true) || (wpm != _wpm))) {
         ret = system("killall espeak >/dev/null 2>&1");
@@ -51,18 +95,72 @@ void Speech::speak(const char *message, bool immediate, int wpm)
     }
 
     if (_espeak == NULL) {
-        snprintf(cmd, sizeof(cmd) - 1,
-                 "/usr/bin/espeak -s %d 2>/dev/null", wpm);
         _espeak = popen(cmd, "w");
         if (_espeak == NULL) {
             perror(cmd);
-            return;
+            goto done;
         }
     }
 
     fwrite(message, 1, strlen(message), _espeak);
     fwrite("\n", 1, 1, _espeak);
     fflush(_espeak);
+#endif
+
+#if defined(ESPEAK_FORK)
+    if ((_pid != -1) &&
+        ((immediate == true) || (wpm != _wpm))) {
+        int wstatus;
+
+        close(_stdin[1]);
+        _stdin[1] = -1;
+
+        ret = kill(_pid, SIGKILL);
+        do {
+            (void)(ret);
+            waitpid(_pid, &wstatus, 0);
+        } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+        _pid = -1;
+    }
+
+    if (_pid == -1) {
+        ret = pipe(_stdin);
+        if (ret != 0) {
+            goto done;
+        }
+
+        _pid = fork();
+        if (_pid == -1) {
+            close(_stdin[0]);
+            close(_stdin[1]);
+            goto done;
+        } else if (_pid == 0) {
+            /* Child */
+            atexit(atexit_do_nothing);
+            signal(SIGTERM, NULL);
+            signal(SIGINT, NULL);
+
+            close(_stdin[1]);
+            dup2(_stdin[0], STDIN_FILENO);
+
+            ret = execl("/bin/sh", "sh", "-c", cmd, NULL);
+            exit(ret);
+        } else {
+            /* Parent */
+            close(_stdin[0]);
+            _stdin[0] = -1;
+        }
+    }
+
+    ret = write(_stdin[1], message, strlen(message));
+    (void)(ret);
+    ret = write(_stdin[1], "\n", 1);
+    (void)(ret);
+#endif
+
+done:
+
+    pthread_mutex_unlock(&_mutex);
 }
 
 unsigned int Speech::volume(void) const
@@ -89,6 +187,8 @@ void Speech::setVolume(unsigned int vol)
     const char *card = "default";
     const char *selem_name = "PCM";
     snd_mixer_elem_t *elem = NULL;
+
+    pthread_mutex_lock(&_mutex);
 
     if (vol > 100) {
         vol = 100;
@@ -148,6 +248,8 @@ done:
     if (handle) {
         snd_mixer_close(handle);
     }
+
+    pthread_mutex_unlock(&_mutex);
 }
 
 /*
--- controller/speech.hxx
+++ controller/speech.hxx
@@ -7,6 +7,9 @@
 #ifndef SPEECH_HXX
 #define SPEECH_HXX
 
+//#define ESPEAK_POPEN
+#define ESPEAK_FORK
+
 class Speech {
 
 public:
@@ -14,16 +17,25 @@ public:
     Speech();
     ~Speech();
 
-    void speak(const char *message, bool immediate = true, int wpm = -1);
+    void speak(const char *message, bool immediate = false, int wpm = -1);
 
     unsigned int volume(void) const;
     void setVolume(unsigned int vol);
 
 private:
 
+#if defined(ESPEAK_POPEN)
+    FILE *_espeak;
+#endif
+#if defined(ESPEAK_FORK)
+    pid_t _pid;
+    int _stdin[2];
+#endif
+
     unsigned int _vol;
     int _wpm;
-    FILE *_espeak;
+
+    pthread_mutex_t _mutex;
 
 };
 
