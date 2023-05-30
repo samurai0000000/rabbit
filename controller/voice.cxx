210d24f39d3c6b0f0e20408edb942efe6abee791
Charles Chiou <samurai@selfso.com>
2023-05-30 09:33:54 +0800

    Implement USB control transfer driver for ReSpeaker
    Signed-off-by: Charles Chiou <samurai@selfso.com>

--- controller/voice.cxx
+++ controller/voice.cxx
@@ -7,13 +7,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/time.h>
+#include <bsd/sys/time.h>
 #include <alsa/asoundlib.h>
+#include <libusb-1.0/libusb.h>
 #include "rabbit.hxx"
+
+/*
+ * https://wiki.seeedstudio.com/ReSpeaker_Mic_Array_v2.0/
+ */
 
 #define AUDIO_PCM_INPUT_DEVICE   "plughw:1,0"
 #define AUDIO_PCM_INPUT_FORMAT   SND_PCM_FORMAT_S16_LE
 #define AUDIO_PCM_INPUT_RATE     22050
 #define AUDIO_PCM_INPUT_CHANS    1
+
+#define USB_ENDPOINT_IN	    (LIBUSB_ENDPOINT_IN  | 0x01)
+#define USB_ENDPOINT_OUT    (LIBUSB_ENDPOINT_OUT | 0x01)
+#define USB_TIMEOUT         1000
+
+struct respeaker_ctrl {
+    uint32_t offset;
+    uint32_t value;
+    uint32_t type;
+};
 
 static unsigned int instance = 0;
 
@@ -21,6 +38,8 @@ Voice::Voice()
     : _handle(NULL),
       _rate(AUDIO_PCM_INPUT_RATE),
       _enable(true),
+      _usbctx(NULL),
+      _usbdev(NULL),
       _volHist(),
       _volHistCur(0)
 {
@@ -35,6 +54,7 @@ Voice::Voice()
     pthread_mutex_init(&_mutex, NULL);
     pthread_cond_init(&_cond, NULL);
     pthread_create(&_thread, NULL, Voice::thread_func, this);
+    pthread_create(&_thread2, NULL, Voice::thread_func2, this);
 
     printf("Voice is online\n");
 }
@@ -44,6 +64,7 @@ Voice::~Voice()
     _running = false;
     pthread_cond_broadcast(&_cond);
     pthread_join(_thread, NULL);
+    pthread_join(_thread2, NULL);
     pthread_mutex_destroy(&_mutex);
     pthread_cond_destroy(&_cond);
 
@@ -52,17 +73,27 @@ Voice::~Voice()
         _handle = NULL;
     }
 
+    if (_usbdev) {
+        libusb_close(_usbdev);
+        _usbdev = NULL;
+    }
+
+    if (_usbctx) {
+        libusb_exit(_usbctx);
+        _usbctx = NULL;
+    }
+
     instance--;
     printf("Voice is offline\n");
 }
 
-void Voice::probeOpenDevice(void)
+void Voice::probeOpenAudioDevice(void)
 {
-    int ret;
+    int ret = 0;
     snd_pcm_hw_params_t *hw_params = NULL;
 
     if (_handle != NULL) {
-        return;
+        goto done;
     }
 
     ret = snd_pcm_open((snd_pcm_t **) &_handle,
@@ -170,7 +201,7 @@ void Voice::run(void)
 
     while (_running) {
         /* Probe and open audio input device */
-        probeOpenDevice();
+        probeOpenAudioDevice();
         if (_handle == NULL) {
             clock_gettime(CLOCK_REALTIME, &ts);
             ts.tv_sec += 1;
@@ -238,6 +269,234 @@ void Voice::run(void)
     if (buf) {
         free(buf);
         buf = NULL;
+    }
+}
+
+void Voice::probeOpenUSBDevice(void)
+{
+    int ret;
+
+    if (_usbctx == NULL) {
+        ret = libusb_init(&_usbctx);
+        if (ret != LIBUSB_SUCCESS) {
+            fprintf(stderr, "libusb_init failed: %s\n",
+                    libusb_strerror(ret));
+            goto done;
+        }
+    }
+
+    if (_usbdev == NULL) {
+        _usbdev = libusb_open_device_with_vid_pid(_usbctx, 0x2886, 0x0018);
+        if (_usbdev == NULL) {
+            ret = -1;
+            goto done;
+        }
+    }
+
+done:
+
+    if (ret != 0 && _usbdev != NULL) {
+        libusb_close(_usbdev);
+        _usbdev = NULL;
+    }
+}
+
+uint32_t Voice::readUsbIntReg(unsigned int id, unsigned int offset)
+{
+    int ret;
+    uint16_t cmd;
+    uint32_t response = 0;
+
+    if (_usbdev == NULL) {
+        return response;
+    }
+
+    cmd = 0x80 | 0x40 | (uint8_t) offset;
+    ret = libusb_control_transfer(_usbdev,
+                                  LIBUSB_REQUEST_TYPE_VENDOR |
+                                  LIBUSB_ENDPOINT_IN,
+                                  0,
+                                  cmd,
+                                  (uint16_t) id,
+                                  (unsigned char *) &response,
+                                  sizeof(response),
+                                  USB_TIMEOUT);
+    if (ret < 0) {
+        fprintf(stderr, "libusb_control_transfer: %s\n",
+                libusb_strerror(ret));
+        libusb_close(_usbdev);
+        _usbdev = NULL;
+    }
+
+    return response;
+}
+
+void Voice::writeUsbIntReg(unsigned int id, unsigned int offset,
+                           uint32_t value)
+{
+    int ret;
+    uint32_t cmd[3];
+
+    if (_usbdev == NULL) {
+        return;
+    }
+
+    cmd[0] = offset;
+    cmd[1] = value;
+    cmd[2] = 1;
+    ret = libusb_control_transfer(_usbdev,
+                                  LIBUSB_REQUEST_TYPE_VENDOR |
+                                  LIBUSB_ENDPOINT_OUT,
+                                  0,
+                                  0,
+                                  (uint16_t) id,
+                                  (unsigned char *) cmd,
+                                  sizeof(cmd),
+                                  USB_TIMEOUT);
+    if (ret < 0) {
+        fprintf(stderr, "libusb_control_transfer: %s\n",
+                libusb_strerror(ret));
+        libusb_close(_usbdev);
+        _usbdev = NULL;
+    }
+}
+
+void Voice::writeUsbFloatReg(unsigned int id, unsigned int offset,
+                             float value)
+{
+    int ret;
+    uint32_t cmd[3];
+
+    if (_usbdev == NULL) {
+        return;
+    }
+
+    cmd[0] = offset;
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
+    cmd[1] = *((const uint32_t *) ((const void *) &value));
+#pragma GCC diagnostic pop
+    cmd[2] = 0;
+    ret = libusb_control_transfer(_usbdev,
+                                  LIBUSB_REQUEST_TYPE_VENDOR |
+                                  LIBUSB_ENDPOINT_OUT,
+                                  0,
+                                  0,
+                                  (uint16_t) id,
+                                  (unsigned char *) cmd,
+                                  sizeof(cmd),
+                                  USB_TIMEOUT);
+    if (ret < 0) {
+        fprintf(stderr, "libusb_control_transfer: %s\n",
+                libusb_strerror(ret));
+        libusb_close(_usbdev);
+        _usbdev = NULL;
+    }
+}
+
+float Voice::readUsbFloatReg(unsigned int id, unsigned int offset)
+{
+    int ret;
+    uint16_t cmd;
+    int32_t response[2] = { 0, 0, };
+
+    if (_usbdev == NULL) {
+        return 0.0;
+    }
+
+    cmd = 0x80 | (uint8_t) offset;
+    ret = libusb_control_transfer(_usbdev,
+                                  LIBUSB_REQUEST_TYPE_VENDOR |
+                                  LIBUSB_ENDPOINT_IN,
+                                  0,
+                                  cmd,
+                                  (uint16_t) id,
+                                  (unsigned char *) response,
+                                  sizeof(response),
+                                  USB_TIMEOUT);
+    if (ret < 0) {
+        fprintf(stderr, "libusb_control_transfer: %s\n",
+                libusb_strerror(ret));
+        libusb_close(_usbdev);
+        _usbdev = NULL;
+    }
+
+    return ((float) response[0] * powf(2.0, (float) response[1]));
+}
+
+void *Voice::thread_func2(void *args)
+{
+    Voice *voice = (Voice *) args;
+
+    voice->run2();
+
+    return NULL;
+}
+
+void Voice::run2(void)
+{
+    struct timespec ts, tloop;
+
+    tloop.tv_sec = 0;
+    tloop.tv_nsec = 500000000;
+
+    while (_running) {
+        /* Probe and open USB device */
+        probeOpenUSBDevice();
+        if (_usbdev == NULL) {
+            clock_gettime(CLOCK_REALTIME, &ts);
+            ts.tv_sec += 1;
+            pthread_mutex_lock(&_mutex);
+            pthread_cond_timedwait(&_cond, &_mutex, &ts);
+            pthread_mutex_unlock(&_mutex);
+            continue;
+        }
+
+        _prop.AECFreezeOnOff = readUsbIntReg(18, 7);
+        _prop.AECNorm = readUsbFloatReg(18, 19);
+        _prop.AECPathChange = readUsbIntReg(18, 25);
+        _prop.RT60 = readUsbFloatReg(18, 26);
+        _prop.HPFOnOff = readUsbIntReg(18, 27);
+        _prop.AECSilenceLevel = readUsbFloatReg(18, 30);
+        _prop.AECSilenceMode = readUsbIntReg(18, 31);
+        _prop.AGCOnOff = readUsbIntReg(19, 0);
+        _prop.AGCMaxGain = readUsbFloatReg(19, 1);
+        _prop.AGCDesiredLevel = readUsbFloatReg(19, 2);
+        _prop.AGCGain = readUsbFloatReg(19, 3);
+        _prop.AGCTime = readUsbFloatReg(19, 4);
+        _prop.CNIOnOff = readUsbIntReg(19, 5);
+        _prop.FreezeOnOff = readUsbIntReg(19, 6);
+        _prop.StatNoiseOnOff = readUsbIntReg(19, 8);
+        _prop.GammaNS = readUsbFloatReg(19, 9);
+        _prop.MinNS = readUsbFloatReg(19, 10);
+        _prop.NonStatNoiseOnOff = readUsbIntReg(19, 11);
+        _prop.GamaNN = readUsbFloatReg(19, 12);
+        _prop.MinNN = readUsbFloatReg(19, 13);
+        _prop.EchoOnOff = readUsbIntReg(19, 14);
+        _prop.GammaE = readUsbFloatReg(19, 15);
+        _prop.GammaETail = readUsbFloatReg(19, 16);
+        _prop.GammaENL = readUsbFloatReg(19, 17);
+        _prop.NLAttenOnOff = readUsbIntReg(19, 18);
+        _prop.NLAECMode = readUsbIntReg(19, 20);
+        _prop.SpeechDetected = readUsbIntReg(19, 22);
+        _prop.FSBUpdated = readUsbIntReg(19, 23);
+        _prop.FSBPathChange = readUsbIntReg(19, 24);
+        _prop.TransientOnOff = readUsbIntReg(19, 29);
+        _prop.VoiceActivity = readUsbIntReg(19, 32);
+        _prop.StatNoiseOnOffSR = readUsbIntReg(19, 33);
+        _prop.NonStatNoiseOnOffSR = readUsbIntReg(19, 34);
+        _prop.GammaNSSR = readUsbFloatReg(19, 35);
+        _prop.GammaNNSR = readUsbFloatReg(19, 36);
+        _prop.MinNSSR = readUsbFloatReg(19, 37);
+        _prop.MinNNSR = readUsbFloatReg(19, 38);
+        _prop.GammaVADSR = readUsbFloatReg(19, 39);
+        _prop.DOAAngle = readUsbIntReg(21, 0);
+
+        clock_gettime(CLOCK_REALTIME, &ts);
+        timespecadd(&ts, &tloop, &ts);
+        pthread_mutex_lock(&_mutex);
+        pthread_cond_timedwait(&_cond, &_mutex, &ts);
+        pthread_mutex_unlock(&_mutex);
     }
 }
 
--- controller/voice.hxx
+++ controller/voice.hxx
@@ -9,12 +9,59 @@
 
 #define VOL_HIST_SIZE 100
 
+struct libusb_context;
+struct libusb_device_handle;
+
 struct vol_hist_point {
     int16_t min;
     int16_t max;
 };
 
 class Voice {
+
+public:
+
+    struct prop {
+        uint32_t AECFreezeOnOff;
+        float    AECNorm;
+        uint32_t AECPathChange;
+        float    RT60;
+        uint32_t HPFOnOff;
+        float    AECSilenceLevel;
+        uint32_t AECSilenceMode;
+        uint32_t AGCOnOff;
+        float    AGCMaxGain;
+        float    AGCDesiredLevel;
+        float    AGCGain;
+        float    AGCTime;
+        uint32_t CNIOnOff;
+        uint32_t FreezeOnOff;
+        uint32_t StatNoiseOnOff;
+        float    GammaNS;
+        float    MinNS;
+        uint32_t NonStatNoiseOnOff;
+        float    GamaNN;
+        float    MinNN;
+        uint32_t EchoOnOff;
+        float    GammaE;
+        float    GammaETail;
+        float    GammaENL;
+        uint32_t NLAttenOnOff;
+        uint32_t NLAECMode;
+        uint32_t SpeechDetected;
+        uint32_t FSBUpdated;
+        uint32_t FSBPathChange;
+        uint32_t TransientOnOff;
+        uint32_t VoiceActivity;
+        uint32_t StatNoiseOnOffSR;
+        uint32_t NonStatNoiseOnOffSR;
+        float    GammaNSSR;
+        float    GammaNNSR;
+        float    MinNSSR;
+        float    MinNNSR;
+        float    GammaVADSR;
+        uint32_t DOAAngle;
+    };
 
 public:
 
@@ -29,21 +76,38 @@ public:
     unsigned int getVolHist(struct vol_hist_point *hist,
                             unsigned int points) const;
 
+    const Voice::prop &getProp(void) const;
+
 private:
 
-    void probeOpenDevice(void);
+    void probeOpenAudioDevice(void);
     static void *thread_func(void *args);
     void run(void);
+
+    void probeOpenUSBDevice(void);
+    uint32_t readUsbIntReg(unsigned int id, unsigned int offset);
+    float readUsbFloatReg(unsigned int id, unsigned int offset);
+    void writeUsbIntReg(unsigned int id, unsigned int offset,
+                        uint32_t value);
+    void writeUsbFloatReg(unsigned int id, unsigned int offset,
+                          float value);
+    static void *thread_func2(void *args);
+    void run2(void);
 
     void *_handle;
     unsigned int _rate;
     bool _enable;
+    libusb_context *_usbctx;
+    libusb_device_handle *_usbdev;
 
     struct vol_hist_point _volHist[VOL_HIST_SIZE];
     unsigned int _volHistCur;
 
+    struct prop _prop;
+
     bool _running;
     pthread_t _thread;
+    pthread_t _thread2;
     pthread_mutex_t _mutex;
     pthread_cond_t _cond;
 
@@ -62,6 +126,11 @@ inline bool Voice::isOnline(void) const
 inline size_t Voice::volHistSize(void) const
 {
     return VOL_HIST_SIZE;
+}
+
+inline const Voice::prop &Voice::getProp(void) const
+{
+    return _prop;
 }
 
 #endif
