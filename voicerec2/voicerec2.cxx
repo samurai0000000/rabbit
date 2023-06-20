b2035d86de1aaef54e00c81ceb791730bf1a3d57
Charles Chiou <samurai@selfso.com>
2023-06-20 23:11:38 +0800

    Preliminary success in voicerec2 using whisper.cpp
    Signed-off-by: Charles Chiou <samurai@selfso.com>

--- voicerec2/voicerec2.cxx
+++ voicerec2/voicerec2.cxx
@@ -8,14 +8,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <vector>
 #include <filesystem>
 #include <whisper.h>
 #include <mosquitto.h>
 
 using namespace std;
 
-struct whisper_context *whisper = NULL;
-struct mosquitto *mosq = NULL;
+#define WHISPER_SAMPLE_RATE 16000
+#define MAIN_SAMPLE_SECONDS 5
+
+struct audio_sample_main {
+    float data[WHISPER_SAMPLE_RATE * MAIN_SAMPLE_SECONDS];
+    unsigned int samples;
+};
+
+static struct whisper_context *whisper = NULL;
+static struct mosquitto *mosq = NULL;
+
+static struct audio_sample_main audio_sample_main = {
+    .data = { 0, },
+    .samples = 0,
+};
+
+static void make_inference(void);
 
 static void on_connect(struct mosquitto *mosq, void *obj, int reason_code)
 {
@@ -82,6 +98,29 @@ static void on_message(struct mosquitto *mosq, void *obj,
     (void)(obj);
 
     if (strcmp(msg->topic, "rabbit/voice/pcm") == 0) {
+        unsigned int new_samples;
+
+        new_samples = msg->payloadlen / sizeof(int16_t);
+        if ((audio_sample_main.samples + new_samples) <=
+            (WHISPER_SAMPLE_RATE * MAIN_SAMPLE_SECONDS)) {
+            unsigned int i;
+            const int16_t *pcm_s16_le = (const int16_t *) msg->payload;
+            float v;
+
+            /* Convert from S16_LE to F32 */
+            for (i = 0; i < new_samples; i++) {
+                v = ((float) *pcm_s16_le) / 32768.0;
+                audio_sample_main.data[audio_sample_main.samples] = v;
+                audio_sample_main.samples++;
+                pcm_s16_le++;
+            }
+        }
+
+        if (audio_sample_main.samples >=
+            (WHISPER_SAMPLE_RATE * MAIN_SAMPLE_SECONDS)) {
+            make_inference();
+            audio_sample_main.samples = 0;
+        }
     } else if (strcmp(msg->topic, "rabbit/voice/state") == 0) {
         const char *str = (const char *) msg->payload;
         if (str != NULL) {
@@ -90,11 +129,50 @@ static void on_message(struct mosquitto *mosq, void *obj,
                 /* No need to do anything */
             } else if ((msg->payloadlen == 3) &&
                        (str[0] == 'o') && (str[1] == 'f') && (str[2] == 'f')) {
+                make_inference();
+                audio_sample_main.samples = 0;
             }
         }
     } else {
         fprintf(stderr, "%s: off-topic: %s\n", __func__, msg->topic);
     }
+}
+
+static void make_inference(void)
+{
+    int ret;
+    whisper_full_params wparams =
+        whisper_full_default_params(WHISPER_SAMPLING_GREEDY);
+
+    wparams.print_progress   = false;
+    wparams.print_special    = false;
+    wparams.print_realtime   = false;
+    wparams.print_timestamps = false;
+    wparams.translate        = false;
+    wparams.single_segment   = false;
+    wparams.max_tokens       = 32;
+    wparams.language         = "en";
+    wparams.n_threads        = 4;
+    wparams.audio_ctx        = 0;
+    wparams.speed_up         = false;
+
+    printf("make inference with %u samples\n", audio_sample_main.samples);
+
+    ret = whisper_full(whisper, wparams,
+                       audio_sample_main.data,
+                       audio_sample_main.samples);
+    if (ret != 0) {
+        fprintf(stderr, "failed to process audio (%d)!\n", ret);
+    }
+
+    const int n_segments = whisper_full_n_segments(whisper);
+    for (int i = 0; i < n_segments; ++i) {
+        const char *text = whisper_full_get_segment_text(whisper, i);
+
+        printf("%s", text);
+        fflush(stdout);
+    }
+    printf("\n");
 }
 
 static void cleanup(void)
@@ -157,12 +235,6 @@ int main(int argc, char **argv)
     atexit(cleanup);
     signal(SIGINT, sig_handler);
 
-    ret = mosquitto_lib_init();
-    if (ret != MOSQ_ERR_SUCCESS) {
-        fprintf(stderr, "mosquitto_lib_init failed (%d)!\n", ret);
-        exit(EXIT_FAILURE);
-    }
-
     for (i = 0; model_candidates[i] != NULL; i++) {
         if (filesystem::exists(model_candidates[i])) {
             whisper = whisper_init_from_file(model_candidates[i]);
@@ -175,7 +247,14 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    mosq = mosquitto_new("Rabbit Voice Recognition", true, NULL);
+    ret = mosquitto_lib_init();
+    if (ret != MOSQ_ERR_SUCCESS) {
+        fprintf(stderr, "mosquitto_lib_init failed (%d)!\n", ret);
+        exit(EXIT_FAILURE);
+    }
+
+    mosq = mosquitto_new("Rabbit Voice Recognition using whisper.cpp",
+                         true, NULL);
     if (mosq == NULL) {
         fprintf(stderr, "mosquitto_new failed!\n");
         exit(EXIT_FAILURE);
